3c28e18de637908d09e30e3042133b8cec191c0e
Bernd Strebel <b.strebel@digitec.de>
2016-10-01 15:47:06 +0200

    python pimatic api examples

---
+++ pimatic-query/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+__version__ = '0.1.0'
+__license__ = 'GPL2'
+__author__ = 'Bernd Strebel'
+
+PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
--- pimatic-query/pimatic-query-events.py
+++ pimatic-query/pimatic-query-events.py
@@ -6,15 +6,9 @@ from __future__ import print_function
 
 import os, sys, re, uuid, logging, logging.config, json, codecs, time, datetime, urllib
 
-from pyutils import Options, LogAdapter, strflocal, timestamp, get_logger, log_level
+from pyutils import Options, LogAdapter, strflocal, localfstr, get_logger, log_level
 from pimatic import PimaticAPI
 
-def eventparms(criteria):
-    parms = {}
-    for k in criteria:
-        if criteria[k] is not None:
-            parms['criteria[' + k + ']'] = criteria[k]
-    return parms
 
 def main():
 
@@ -25,10 +19,10 @@ def main():
     node = {'url': None, 'username': None, 'password': None}
 
     options = {
-        'config': '~/.pimatic/pimatic.cfg',
-        'window': 90,
+        'config': '~/.pimatic/pimatic.cfg,~/.pimatic/events.cfg',
+        'window': '90s',
         'device': '.*',
-        'exclude': '(weather|sunrise|forecast|bmp|syssensor)',
+        'exclude': '(var_|weather|sunrise|forecast|bmp|syssensor)',
         'format': '%Y-%d-%m %H:%M:%S',
         'loglevel_requests': 'ERROR',
         'loglevel': 'DEBUG'
@@ -50,12 +44,15 @@ def main():
     parser.add_argument('-d', '--device', type=str, help='device specification (regex)')
     parser.add_argument('-v', '--invert', action='store_true', help='invert regex match')
     parser.add_argument('-x', '--exclude', type=str, help='device specification (regex)')
+
+    # event only options
     parser.add_argument('-a', '--after', type=str, help='after datetime')
     parser.add_argument('-b', '--before', type=str, help='before datetime')
     parser.add_argument('-t', '--time', type=str, help='datetime spec ')
     parser.add_argument('-w', '--window', type=str, help='time window')
     parser.add_argument('-o', '--order', type=str, help='order by attribute (default: time)')
     parser.add_argument('-r', '--reverse', action='store_true', help='reverse sort order')
+
     # parser.add_argument('-f', '--offset', type=str, help='offset')
     # parser.add_argument('-l', '--limit', type=str, help='limit')
 
@@ -66,7 +63,6 @@ def main():
     if config is None:
         LogAdapter(get_logger(), {'package': 'pimatic-query'}).critical("Missing configuration file!")
         exit(1)
-
 
     logger = LogAdapter(opts.logger, {'package': 'pimatic-query'})
 
@@ -114,18 +110,23 @@ def main():
 
 # endregion
 
+    logger.info(u'url: %s' % (node['url']))
+
     with PimaticAPI(server=node['url'],
                     username=node['username'],
                     password=node['password'],
                     logger=logger) as pimatic:
 
-        # generate device list
+# region Process device list
         pattern = None
         exclude = None
         if opts.device:
             pattern = re.compile(opts.device)
+            logger.info(u'device: %s' % (opts.device))
+            logger.info(u'invert: %s' % (opts.invert))
         if opts.exclude:
             exclude = re.compile(opts.exclude)
+            logger.info(u'exclude: %s' % (opts.exclude))
         inverse = opts.invert
         devices = []
         for id in pimatic.devices:
@@ -139,40 +140,112 @@ def main():
                     else:
                         devices.append(id)
         devices.sort()
+        logger.info(u'devices: %d (%d)' % (len(devices), len(pimatic.devices)))
         # for id in devices: print(id)
+# endregion
 
         criteria = {'deviceId': None, 'order': 'time', 'attributeName': None, 'after': None, 'before': None,
                     'orderDirection': None, 'offset': None, 'limit': None}
 
-        if opts.after:
-            criteria['after'] = timestamp(opts.after) * 1000
+# region Process time frame settings
+        if opts.time:
+            time = opts.time
+            delta = opts.window
+            tokens = opts.time.split()
+            if re.match('[0-9,.]+[smhd]', tokens[-1]):
+                delta = tokens[-1]
+                time = " ".join(tokens[0:-1])
+            logger.info('time: %s' % (time))
+            logger.info('delta: %s' % (delta))
+            criteria['after'] = localfstr('{} -{}'.format(time, delta)) * 1000
+            criteria['before'] = localfstr('{} +{}'.format(time, delta)) * 1000
+        else:
+            if opts.after:
+                logger.info('after: %s' % (opts.after))
+                criteria['after'] = localfstr(opts.after) * 1000
+            if opts.before:
+                logger.info('before: %s' % (opts.before))
+                criteria['before'] = localfstr(opts.before) * 1000
 
-        if opts.before:
-            criteria['before'] = timestamp(opts.before) * 1000
+        if criteria['after']:
+            logger.info('after: %s' % strflocal(criteria['after']))
+        if criteria['before']:
+            logger.info('before: %s' % strflocal(criteria['before']))
+# endregion
 
         if len(devices) == 1:
             criteria['deviceId'] = devices[0]
 
-        events = []
+        logger.debug(u'criteria %s' % criteria)
 
-        result = pimatic.get('/api/database/device-attributes/?' + urllib.urlencode(eventparms(criteria)))
+        result = pimatic.get('/api/database/device-attributes/?' +
+                             urllib.urlencode(pimatic.eventparms(criteria)))
+
         if pimatic.check_result(result,'events'):
+
+            events = []
+            width = {'deviceId': 0, 'attributeName': 0, 'type': 0, 'cname': 0, 'label': 0}
+
+            def events_append(event):
+
+                if pimatic.devices.get(event['deviceId']):
+
+                    device = pimatic.devices[event['deviceId']]
+                    event['cname'] = device['config']['class']
+                    for attribute in device['attributes']:
+                        if attribute['name'] == event['attributeName']: break
+                    event['label'] = attribute['label']
+                    if attribute['type'] == 'boolean':
+                        event['value'] = attribute['labels'][int(not event['value'])]
+
+                    if not opts.get('pir_absent', True):
+                        if event['cname'] == u'HomeduinoRFPir' and event['value'] == u'absent':
+                            return
+
+                for k in width:
+                    width[k] = max(width[k], len(event[k]))
+
+                events.append(event)
+
             for event in result['events']:
+                # this should not be should not be necessary :-(
+                if criteria['after'] and event['time'] <= criteria['after']: continue
+                if criteria['before'] and event['time'] >= criteria['before']: continue
                 id = event['deviceId']
                 if exclude is not None:
                     if not exclude.search(id):
                         if pattern is not None:
                             if inverse:
-                                if not pattern.search(id): events.append(event)
+                                if not pattern.search(id): events_append(event)
                             else:
-                                if pattern.search(id): events.append(event)
+                                if pattern.search(id): events_append(event)
                         else:
-                            events.append(event)
+                            events_append(event)
 
+            logger.info('events: %d (%d)' % (len(events),len(result['events'])))
             for event in events:
-                print(strflocal(event['time']), event['time'], event['deviceId'], event['attributeName'], event['value'])
 
-        # deviceId, attributeName, type, time, value
+                # long = u'{} {:{device}} {:{cname}} {:{attribute}} {:{label}} {:{type}} {}'.format(
+                #     strflocal(event['time']),
+                #     event['deviceId'],
+                #     event['cname'],
+                #     event['attributeName'],
+                #     event['label'],
+                #     event['type'],
+                #     event['value'],
+                #     device=width['deviceId'],cname=width['cname'], attribute=width['attributeName'], label=width['label'], type=width['type']
+                # )
+
+                short = u'{} {:{device}} {:{attribute}} {}'.format(
+                    strflocal(event['time']),
+                    event['deviceId'],
+                    event['attributeName'],
+                    event['value'],
+                    device=width['deviceId'], attribute=width['attributeName']
+                )
+
+                print(short)
+
 
 # region __Main__
 
